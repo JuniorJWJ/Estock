494f99c33f969b9b344711ef2de5e59d3bd60d29
JuniorJWJ <54831275+JuniorJWJ@users.noreply.github.com>
2020-03-08 22:47:34 -0300

    Arquivo Estoque

--- estoque.php
+++ estoque.php
@@ -20,7 +20,7 @@
 	<body>
 	<div class="container theme-showcase" role="main">
 			<div class="page-header">
-				<h1>Listar Produtos</h1>
+				<h1>Estoque</h1>
 			</div>
 			<div class="pull-right">
 				<button type="button" class="btn btn-xs btn-success" data-toggle="modal" data-target="#myModalcad">Cadastrar Produto</button>
@@ -53,11 +53,6 @@
 									<input name="valor" type="text" class="form-control" id = "valor1" >
 								</div>
 								<div class="form-group">
-									<label for="id11123" class="control-label">Quantidade :</label> <br>			<!-- DESCRIÇÃO -->
-									<input type="hidden" id="id11123" name="id" value="<?php echo $rows_produto['id']; ?>"> <!-- QUANTIDADE -->
-                                    <input type="number" id="quantity" value="<?php echo $rows_produto['quantidade']; ?>" name="quantidade">   
-								</div>
-								<div class="form-group">
 									Categoria do Produto:
 									<select name="categoria" class="form-control" id="exampleFormControlSelect1">
 									<option>Selecione</option>
@@ -70,7 +65,6 @@
 										?>
 								</select><br><br>
 								</div>
-								
 								<input type="file" name="imagem" id="imagem" onchange="previewImagem()"><br><br>
 								<img style="width: 150px; height: 150px;"><br><br> 									<!-- FOTO -->
 								<div class="modal-footer">
@@ -121,7 +115,8 @@
 							<tr>
 								<th>#</th>
 								<th>Nome do Produto</th>
-								<th>Ação</th>
+                                <th>Código de Barras</th>
+								<th>Quantidade</th>
 							</tr>
 						</thead>
 						<tbody>
@@ -129,71 +124,17 @@
 								<tr>
 									<td><?php echo $rows_produto['id']; ?></td>
 									<td><?php echo $rows_produto['nome']; ?></td>
-									<td>
-										<button type="button" class="btn btn-xs btn-primary" data-toggle="modal" data-target="#myModal<?php echo $rows_produto['id']; ?>">Visualizar</button>
-										<button type="button" class="btn btn-xs btn-warning" data-toggle="modal" data-target="#exampleModal" 
-											data-whatever="<?php echo $rows_produto['id']; ?>" 
-											data-whatevervalor="<?php echo $rows_produto['valor']; ?>"
-											data-whateverfoto="<?php echo $rows_produto['foto']; ?>"  
-											data-whatevercodigo_barras="<?php echo $rows_produto['codigo_barras']; ?>" 
-											data-whatevernome="<?php echo $rows_produto['nome']; ?>"
-											data-whateverdetalhes="<?php echo $rows_produto['detalhe']; ?>">Editar</button>
-										<button type="button" class="btn btn-xs btn-danger"  data-toggle="modal" data-target="#myModalDel<?php echo $rows_produto['id']; ?>" data-whatever="<?php echo $rows_produto['id']; ?>">Apagar</button>
-									</td>
-								</tr>
-								<!-- Inicio Modal EXIBIR -->
-								<div class="modal fade" id="myModal<?php echo $rows_produto['id']; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-									<div class="modal-dialog" role="document">
-										<div class="modal-content">
-											<div class="modal-header">
-												<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-												<h4 class="modal-title text-center" id="myModalLabel"><?php echo $rows_produto['nome']; ?></h4>
-											</div>
-											<div class="modal-body">
-												<p><?php echo "ID : ".$rows_produto['id']; ?></p><hr>
-												<p><?php echo "Nome : ".$rows_produto['nome']; ?></p><hr>
-												<p><?php echo "Descrição : ".$rows_produto['detalhe']; ?></p><hr>
-												<p><?php echo "Código de Barras : ".$rows_produto['codigo_barras']; ?></p><hr>
-												<p><?php echo "Preço : ".$rows_produto['valor'	]; ?></p><hr>
-												<p><?php $teste = $rows_produto['fk_categoria']; 
-													$teste1 = "SELECT nome FROM categoria WHERE id='$teste'";
-													$result = mysqli_query($conn, $teste1);
-													$row_categoria1 = mysqli_fetch_assoc($result);
-													echo "Categoria : ".$row_categoria1['nome'];
-												?></p><hr>
-												
+                                    <td><?php echo $rows_produto['codigo_barras']; ?></td>
+                                    <td>
+                                    <form method="POST" action="http://localhost/estock1/processa_quantidade.php" enctype="multipart/form-data">
+                                        <div class="form-group">
+                                            <input type="hidden" id="id11123" name="id" value="<?php echo $rows_produto['id']; ?>">
+                                            <input type="number" id="quantity" value="<?php echo $rows_produto['quantidade']; ?>" name="quantidade">
+                                            <input type="submit" value ="Alterar quantidade"></td>
+                                        </div>
+                                    </form>
 
-												<p><?php //echo "Categoria : ".$rows_produto['fk_categoria']; ?></p>
-												<img src="<?php echo "upload/".$rows_produto['Foto'] ?>" style="width: 150px; height: 150px;"><br><br>
-											</div>
-										</div>
-									</div>
-								</div>
-								<!-- Fim Modal -->
-
-                                <!-- MODAL APAGAR-->
-								<div class="modal fade" id="myModalDel<?php echo $rows_produto['id']; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-									<div class="modal-dialog" role="document">
-										<div class="modal-content">
-											<div class="modal-header">
-												<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-												<h4 class="modal-title text-center" id="myModalLabel"><?php echo $rows_produto['nome']; ?></h4>
-											</div>
-											<div class="modal-body">
-												<p>Tem certeza que deseja apagar o <?php echo "  ".$rows_produto['nome']; ?> ?</p>
-												<form method="POST" action="http://localhost/estock1/proc_apagar_usuario.php" enctype="multipart/form-data">
-														<input name="id" type="hidden" class="form-control" id="id-curso" value="">
-														<button type="button" class="btn btn-success" data-dismiss="modal">Cancelar</button>
-														<button type="submit" class="btn btn-danger">Apagar</button>
-														<?php echo "<a href='proc_apagar_usuario.php?id=".$rows_produto['id'] ."'>   Apagar</a><br><hr>";?>
-												
-												</form>
-											</div>
-										</div>
-									</div>
-								</div>
-
-								<!-- fim modal apagar  -->
+								</tr>   
 							<?php } ?>
 						</tbody>
 					 </table>
@@ -204,53 +145,7 @@
 		
 		
 
-		<!-- inicio modal ALTERAR  -->
-		<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel">
-		  <div class="modal-dialog" role="document">
-			<div class="modal-content">
-			  <div class="modal-header">
-				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-				<h4 class="modal-title" id="exampleModalLabel">Curso</h4>
-			  </div>
-
-			  <div class="modal-body">
-				<form method="POST" action="http://localhost/estock1/processa_editar_usuario.php" enctype="multipart/form-data">
-					
-				  	<div class="form-group">
-						<label for="recipient-name" class="control-label">Nome:</label>
-						<input name="nome" type="text" class="form-control" id="recipient-name">
-				  	</div>
-
-				  	<div class="form-group">
-						<label for="detalhes" class="control-label">Detalhes:</label>
-						<textarea name="detalhes" class="form-control" id="detalhes"></textarea>
-				  	</div>
-
-					<div class="form-group">
-						<label for="codigo_barras" class="control-label">Código de Barras :</label>
-						<input name="codigo_barras" type="text" class="form-control" id="codigo_barras">
-					</div>
-					<div class="form-group">
-						<label for="valor" class="control-label">Valor :</label>   			<!-- VALOR -->
-						<input name="valor" type="text" class="form-control" id="valor">
-					</div>
-					<input type="file" name="imagem" id="imagem" onchange="previewImagem()"><br><br>
-								<!-- <img src="teste1.jpg"style="width: 150px; height: 150px;"><br><br> 		 -->
-								<img src="<?php echo "upload/".$rows_produto['id']."1" ?>" style="width: 150px; height: 150px;"><br><br>							<!-- FOTO -->
-					
-					<input name="id" type="hidden" class="form-control" id="id-curso" value="">
-					
-					<button type="button" class="btn btn-success" data-dismiss="modal">Cancelar</button>
-					<button type="submit" class="btn btn-danger">Alterar</button>
-			 
-				</form>
-			  </div>
-			  
-			</div>
-
-		  </div>
-		</div>
-		<!-- fim modal alterar  -->
+		
 
     <!-- jQuery-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
@@ -258,6 +153,7 @@
     <!-- Include plugins -->
     <script src="js/bootstrap.min.js"></script>
 	<script type="text/javascript">
+    
 		$('#exampleModal').on('show.bs.modal', function (event) {
 		  var button = $(event.relatedTarget) // Button that triggered the modal
 		  var recipient = button.data('whatever') // Extract info from data-* attributes
---
+++ processa_quantidade.php
@@ -0,0 +1,34 @@
+<?php
+	session_start();
+    include_once("conexao.php");
+    $id = mysqli_real_escape_string($conn, $_POST['id']);
+    $quantidade = mysqli_real_escape_string($conn, $_POST['quantidade']);
+    
+    $result_produtos = "UPDATE produto SET quantidade = '$quantidade' WHERE id = '$id'";
+    $resultado_produtos = mysqli_query($conn, $result_produtos);
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+	</head>
+
+	<body> <?php
+		if(mysqli_affected_rows($conn) != 0){
+			echo "
+				<META HTTP-EQUIV=REFRESH CONTENT = '0;URL=http://localhost/estock1/estoque.php'>
+				<script type=\"text/javascript\">
+					alert(\"Curso alterado com Sucesso.\");
+				</script>
+			";	
+		}else{
+			echo "
+				<META HTTP-EQUIV=REFRESH CONTENT = '0;URL=http://localhost/estock1/estoque.php'>
+				<script type=\"text/javascript\">
+					alert(\"Curso não foi alterado com Sucesso.\");
+				</script>
+			";	
+		}?>
+	</body>
+</html>
+<?php $conn->close(); ?>
