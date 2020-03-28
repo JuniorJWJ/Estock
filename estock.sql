1b99d7a6f6cf18d4e9f35bd1698e3c94d46cde1a
Alisson Santana <alissonsantanapo@gmail.com>
2020-03-27 22:07:29 -0300

    Update estock.sql

--- estock.sql
+++ estock.sql
@@ -28,6 +28,9 @@ SET time_zone = "+00:00";
 -- Estrutura da tabela `cargo`
 --
 
+CREATE DATABASE estock;
+USE estock;
+
 CREATE TABLE `cargo` (
   `id` int(11) NOT NULL,
   `nome` varchar(20) NOT NULL
